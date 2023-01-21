83f1170435db5124d5f5b0c1b6c980d42c9cc676
ismilearefin <arefinismile@gmail.com>
2023-01-21 18:54:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid justify-center items-center min-w-full min-h-screen">
+      <form>
+       <label><p>Your Name</p></label>
+       <input type="text"  className="input input-bordered w-96 " />
+       <label><p>Your Text</p></label>
+       <textarea   className="input input-bordered w-96 h-24" />
+       <Link to="/table"><button className="btn btn-info block mt-4 w-1/2 text-center">Submit</button></Link>
+      </form>
     </div>
   );
 }
---
+++ src/Route/Route.js
@@ -0,0 +1,18 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import App from "../App";
+import TableRoute from "../TableRoute/TableRoute";
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App></App>,
+    },
+    {
+        path: "/table",
+        element: <TableRoute></TableRoute>,
+    }
+  ]);
